8d6fe125b8d24f08a3b12a11e965005198131d3e
Ivan Tolkach <ivantolkach2005@gmail.com>
2026-02-16 13:06:34 +0300

    Load of unversioned files

---
+++ src/main/java/dev/tolkach/psychologicalTestsApplication/PsychologicalTestsApplication.java
@@ -0,0 +1,13 @@
+package dev.tolkach.psychologicalTestsApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologicalTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tolkach/psychologicalTestsApplication/PsychologicalTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolkach.psychologicalTestsApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
